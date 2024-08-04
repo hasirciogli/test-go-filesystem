5fc9ae0e577f05bc957622a6a30ebcbd160f2567
Mustafa Hasırcıoğlu <mhasirciogli@gmail.com>
2024-08-04 16:58:24 +0300

    sasa

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/hasirciogli/test-go-filesystem/server"
 )
 
 func HomeHandler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Println(server.CreateServer())
+	writer.Header().Add("content-type", "video/mp4")
+	writer.Write([]byte("here is a string dude"))
 }
 func ProductsHandler(writer http.ResponseWriter, request *http.Request) {
 
@@ -25,4 +25,9 @@ func main() {
 	r.HandleFunc("/products", ProductsHandler)
 	r.HandleFunc("/articles", ArticlesHandler)
 	http.Handle("/", r)
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatalln("There's an error with the server", err)
+	}
 }
