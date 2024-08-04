ba3d2cc8621528939f154e353ed2a35b4523d8ce
Mustafa Hasırcıoğlu <mhasirciogli@gmail.com>
2024-08-04 14:16:34 +0300

    123413

--- hole.go
+++ hole.go
@@ -1,5 +1,26 @@
 package main
 
-func requestYarrak() {
+import (
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func HomeHandler(writer http.ResponseWriter, request *http.Request) {
+
+}
+func ProductsHandler(writer http.ResponseWriter, request *http.Request) {
+
+}
+func ArticlesHandler(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/products", ProductsHandler)
+	r.HandleFunc("/articles", ArticlesHandler)
+	http.Handle("/", r)
 }
